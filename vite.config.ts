ea1d2a246f2977d5653b3c2a18a058d0a1931cee
ThanilkaUdugama <thanilkavishvajith@gmail.com>
2024-12-30 14:06:32 +0530

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/youtube-playlist-wizard/', // Replace 'your-repo-name' with your actual repository name
   plugins: [react()],
-})
+});
